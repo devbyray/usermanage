d017bd5fcdab587e5e9e4d15959f8caaf7cb99ba
Raymon <raymon@internetstrategiecoaching.nl>
2014-03-17 13:56:23 +0100

    Building a Sails app EP7

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,6 +58,7 @@ module.exports = function (grunt) {
 
     // *->    put other dependencies here   <-*
     'linker/js/jquery.js',
+    'linker/js/jquery.validate.min.js',
     'linker/js/bootstrap.js',
 
     // All of the rest of your app scripts imported here
